18c0cdb79cffbb8f78692f0de2482974bfafb3d2
swathi3260 <swathi.peesapati@cgi.com>
2018-10-23 17:55:49 +0530

    styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,20 @@ import {HomeComponent} from './home/home.component';
 import {QuestionsComponent} from './questions/questions.component';
 import { AddtechnologyComponent } from './addtechnology/addtechnology.component';
 import { AddtopicComponent } from './addtopic/addtopic.component';
-import { QuestionlibraryComponent } from './questionlibrary/questionlibrary.component';
-import { QuestionformComponent } from './questionform/questionform.component';
+//import { QuestionlibraryComponent } from './questionlibrary/questionlibrary.component';
 import { ProfilepageComponent } from './profilepage/profilepage.component';
+import { PublishedquestionsComponent } from './publishedquestions/publishedquestions.component';
+import { SavedquestionsComponent } from './savedquestions/savedquestions.component';
+
 
 const routes: Routes = [
   {path: '', component: ProfilepageComponent},
   {path:'home',component: HomeComponent},
-  {path: 'questions', component: QuestionsComponent},
-  {path: 'addtechnology', component: AddtechnologyComponent},
-  {path: 'addtopic',component: AddtopicComponent},
-  {path: 'questionlibrary',component: QuestionlibraryComponent},
+  {path: 'home/questions', component: QuestionsComponent},
+  {path: 'home/addtechnology', component: AddtechnologyComponent},
+  {path: 'home/addtopic',component: AddtopicComponent},
+  {path:'publishedquestions',component: PublishedquestionsComponent},
+  {path:'savedquestions',component:SavedquestionsComponent}
 ];
 
 @NgModule({
